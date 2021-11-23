7fc25e03bfb51549e48f5c32ee52af12bfe4db80
Egor Lugovkin <mrpostik@gmail.com>
2021-11-23 20:13:42 +0700

    Реализовал функцию высчитывания расстояния между углами прямоугольников

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ def cg_distance(x1, y1, x2, y2, x3, y3, x4, y4) -> float:
     print(middle_side_a, middle_side_b)
     dist = (middle_side_b[0] - middle_side_a[0]) ** 2 + (middle_side_b[1] - middle_side_a[1]) ** 2
     return round(dist ** 0.5, 2)
+
+
+def corner_distance(x1, y1, x2, y2, x3, y3, x4, y4) -> float:
+    vector_a = x3 - x1, y3 - y1
+    vector_b = x4 - x2, y4 - y2
+    sum_vector = (vector_a[0] ** 2 + vector_a[1] ** 2) ** 0.5 \
+                 + (vector_b[0] ** 2 + vector_b[1] ** 2) ** 0.5
+    return round(sum_vector, 2)
+
