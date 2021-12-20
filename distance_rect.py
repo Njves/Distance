ebc3e7e24828efba82a23354d2403f9034e62fb3
Egor Lugovkin <mrpostik@gmail.com>
2021-12-20 16:46:25 +0700

    Добавил README

--- distance_rect.py
+++ distance_rect.py
@@ -1,26 +1,24 @@
 
 def cg_distance(x1, y1, x2, y2, x3, y3, x4, y4) -> float:
-    """
-    Функция возвращающая расстояние между углами
+    """Функция возвращающая расстояние между углами
+
     :param x1: левая верхняя x коорданата 1 прямоугольника
     :type x1: int
-    :param y1 левая вехння y координата 1 прямоугольника
+    :param y1: левая вехння y координата 1 прямоугольника
     :type y1: int
     :param x2: правая нижняя x координата 1 прямоугольника
     :type x2: int
     :param y2: правая нижняя y координата 1 прямоугольника
     :type y2: int
     :param x3: левая верхння x координата 2 прямоугольника
-    :type x3
+    :type x3: int
     :param y3: левая вехння y координата 2 прямоугольника
     :type y3: int
-    :param x4: int правая нижняя x координата 2 прямоугольника
+    :param x4: правая нижняя x координата 2 прямоугольника
     :type x4: int
     :param y4: правая нижняя y координата 2 прямоугольника
     :type y4: int
-    :return: расстояние между центрами прямоугольника
-    :rtype: float
-    :raise: TypeError when argument not number type
+    :return: float - расстояние между центрами прямоугольника
 
     """
     middle_side_a = (x2 + x1) / 2, (y1 + y2) / 2
@@ -52,7 +50,6 @@ def corner_distance(x1, y1, x2, y2, x3, y3, x4, y4) -> float:
     :return: расстояние между центрами прямоугольника
     :rtype: float
     :raise: TypeError when argument not number type
-
     """
     vector_a = x3 - x1, y3 - y1
     vector_b = x4 - x2, y4 - y2
